f3df15beb14b38398ba475d5b6c835c2dfe500ff
JimcostDev <rjak727@gmail.com>
2021-05-03 10:35:01 -0500

    encontar el tiempo final(hora) de un evento

--- encontrar_tiempo_final.py
+++ encontrar_tiempo_final.py
@@ -1,7 +1,23 @@
 """
-    La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, expresándolo en horas y minutos. Las horas van de 0 a 23 y los minutes de 0 a 59. 
+    La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, 
+    expresándolo en horas y minutos. Las horas van de 0 a 23 y los minutes de 0 a 59. 
 
     Por ejemplo, si el evento comienza a las 12:17 y dura 59 minutos, terminará a las 13:16.
+    
+    Datos de Prueba
+      Entrada de muestra:
+      12
+      17
+      59
+      Salida esperada: 13:16
+
+
+      Entrada de muestra:
+      23
+      58
+      642
+
+      Salida esperada: 10:40
 """
 
 #datos
@@ -17,7 +33,11 @@ suma = min + dura
  
 if suma >= 60:
   dife = suma % 60
-  hora += 1
+  suma_hora = suma // 60
+  hora += suma_hora
+  if hora > 24:
+        hora = 0
+        hora += suma_hora - 1
   print(f"Hora final: {hora}:{dife}")
 else:
     suma = min + dura
