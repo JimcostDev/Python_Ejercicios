e65cd4342e53c0f58e8bb0d96f3c099383a13204
JimcostDev <rjak727@gmail.com>
2022-11-01 16:18:54 +0100

    fisica 2 y S.Os - algoritmo por prioridades

---
+++ algoritmo_planificacion_x_prioridades.py
@@ -0,0 +1,46 @@
+"""
+procesos {
+    key : value,
+    prioridad : rafaga,
+    3, 10
+}
+
+nota: el valor de prioridad (key) debe ser unico
+"""
+from operator import itemgetter
+try:
+    print('💻💻💻💻💻💻 OBTENEMOS DATOS DE ENTRADA 💻💻💻💻💻💻')
+    print()
+    cantidad_procesos = int(input('Ingrese el número de procesos: '))
+    if(cantidad_procesos > 0):
+        procesos = {}
+        for pr in range(cantidad_procesos):
+            print(f'Proceso #{pr + 1}')
+            prioridad = int(input(f'Ingrese el valor de prioridad ⏱️  del proceso #{pr + 1}: '))
+            rafaga= int(input(f'Ingrese el valor de rafaga 🔥 en CPU del proceso #{pr + 1}: '))
+            procesos[prioridad] = rafaga
+            print()
+            print()
+        print('✅✅✅✅✅ ORDENAMOS LOS PROCESOS SEGÚN SU PRIORIDAD ✅✅✅✅✅')
+        # ordenar el diccionario a partir de su llave (prioridad)
+        procesos_ordenados = dict(sorted(procesos.items(), key=itemgetter(0)))
+        #print(procesos_ordenados) 
+        tiempoRespuesta = 0
+        totalTR = []
+        for llave, valor in procesos_ordenados.items():
+            tiempoRespuesta += valor
+            print(f"""
+            Proceso con prioridad: #{llave}
+            Tiempo de respuesta: {tiempoRespuesta}
+            ------------ Fin del proceso ------------
+            """)
+            
+            totalTR.append(tiempoRespuesta)
+
+        sumatoriaTR = sum(totalTR)
+        promedioTR = sumatoriaTR / len(procesos_ordenados)
+        print(f'El tiempo de respuesta promedio es de: {promedioTR} ut.')
+    else:
+       print('El número de procesos debe ser mayor a 0 (cero) para poder probar el algoritmo.')
+except Exception as e:
+    print(e)
