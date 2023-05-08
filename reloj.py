7d59634e8b29c1cf4b8d8f61e0fa579a23cde484
JimcostDev <rjak727@gmail.com>
2023-05-08 19:39:55 +0200

    simulación basica de un reloj

--- reloj.py
+++ reloj.py
@@ -9,20 +9,22 @@ S = 0 # Segundos
 while True:
     # aumentar segundos cada segundo
     S += 1
+    # las instrucciones 'assert'  verifican que los valores de H,M,S sean siempre menores que 24, 60 y 60, respectivamente:
+    assert S < 60
     time.sleep(1)
     if (S == 60):
         S = 0
         M += 1
+        assert M < 60
     if (M == 60):
-        S = 0
         M = 0
         H += 1
+        assert H < 24
     if (H == 24):
-        S = 0
-        M = 0
         H = 0
     # formatear la hora
     hora_formateada = datetime.datetime(1, 1, 1, H, M, S).strftime("%H:%M:%S")
     print(hora_formateada, end="", flush=True)  
     print("\r", end="", flush=True)
+
 
