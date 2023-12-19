edb670ee4e88ecde2987a2cfa1128cea40d67f36
JimcostDev <rjak727@gmail.com>
2023-12-19 12:46:21 +0100

    correciíon en ejercicio de chatgpt

--- ejercicios/chatgpt_desde_python/chatgpt.py
+++ ejercicios/chatgpt_desde_python/chatgpt.py
@@ -11,10 +11,15 @@ while True:
         if prompt == "exit":
             break
 
-        completion = openai.Completion.create(engine="text-davinci-003",
-                                prompt= prompt,
-                                max_tokens=2048)
+        completion = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=2048
+        )
 
         print(completion.choices[0].text)
-    except openai.error.RateLimitError as err:
+    except openai.error.APIError as err:
         print(err)
+    except Exception as e:
+        print("Ocurrió un error:", e)
+
