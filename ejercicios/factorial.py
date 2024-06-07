6c36512fe44b12fb4c73ba0d3d03bf67033b7937
JimcostDev <rjak727@gmail.com>
2024-06-07 13:09:48 +0200

    recursividad - factorial y fibonacci

--- ejercicios/factorial.py
+++ ejercicios/factorial.py
@@ -1,24 +1,26 @@
-# -*- coding: utf-8 -*-
-"""recursión: una función puede ejecutarse a si misma dentro de ella
-5! = 5*4*3*2*1
-4! = 4*3*2*1
-5! = 5*4!
-   4!=4*3!
-    3!=3*2!
-      2!=2*1!
-         1=1*0!"""
-
-
 def factorial(number):
-    if number == 0:
+    """
+    Calcula el factorial de un número entero no negativo.
+
+    :param number: Número al que se le calculará el factorial (debe ser un entero no negativo).
+    :return: El factorial del número.
+
+    Ejemplo:
+    factorial(5) == 120  # Calcula 5! = 5 * 4 * 3 * 2 * 1
+    """
+
+    if number < 0:
+        raise ValueError("El número debe ser un entero no negativo")
+    elif number == 0:
         return 1
+    else:
+        return number * factorial(number - 1)
 
-    return number * factorial(number - 1)
 
-
-if __name__ == '__main__':
-    number = int(input('Escribe un número: '))
-
-    result = factorial(number)
-
-    print(result)
+if __name__ == "__main__":
+    try:
+        number = int(input("Escribe un número entero no negativo: "))
+        result = factorial(number)
+        print(f"El factorial de {number} es {result}")
+    except ValueError as e:
+        print(f"Error: {e}")
