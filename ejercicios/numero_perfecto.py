8991941a4ceb33d72643cf4d7515641792ec2bb3
JimcostDev <rjak727@gmail.com>
2025-01-31 13:00:50 +0100

    version 2, numero perfecto

--- ejercicios/numero_perfecto.py
+++ ejercicios/numero_perfecto.py
@@ -6,13 +6,15 @@ sin contarse el mismo número.
     
 Nota: Hasta la fecha se han encontrado 51 números pefectos 
 """
-# Función que determina si un numero es perdecto o no
+
+## version 1 ##
+# Función que determina si un numero es perfecto o no
 def esPerfecto(numero):
     if numero <= 5:
         return False
     contar = []
-    for n in range(1,numero):
-        if numero%n == 0:
+    for n in range(1, numero):
+        if numero % n == 0:
             contar.append(n)
     suma = sum(contar)
     if suma == numero:
@@ -20,11 +22,10 @@ def esPerfecto(numero):
     else:
         return False
 
-
-def run():
+def run_version_1():
     hasta = int(input('Ingrese el número límite (hasta) para buscar los números perfectos hasta ese número dado: '))
     lista_perfectos = []
-    for n in range(5,hasta):
+    for n in range(5, hasta):
         if esPerfecto(n):
             lista_perfectos.append(n)
 
@@ -32,5 +33,32 @@ def run():
     print(lista_perfectos)
     print(f'Existen {len(lista_perfectos)} números perfectos hasta el número {hasta}.')
 
+## version 2 ##
+def es_numero_perfecto(numero):
+    suma_divisores = sum([i for i in range(1, numero) if numero % i == 0])
+    return suma_divisores == numero
+
+def imprimir_numeros_perfectos(limite):
+    for numero in range(1, limite + 1):
+        if es_numero_perfecto(numero):
+            print(numero)
+
+def run_version_2():
+    limite = int(input("Introduce el límite: "))
+    imprimir_numeros_perfectos(limite)
+
+def main():
+    print("Seleccione la versión a ejecutar:")
+    print("1. Versión 1")
+    print("2. Versión 2")
+    opcion = int(input("Ingrese el número de la versión: "))
+
+    if opcion == 1:
+        run_version_1()
+    elif opcion == 2:
+        run_version_2()
+    else:
+        print("Opción no válida")
+
 if __name__ == '__main__':
-    run()
+    main()
